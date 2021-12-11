5f9f2d04b4c5c8f3399c7db7aac385e9f4d9e2e3
Alimu Dayimu <alimdayim@hotmail.com>
2021-12-11 18:53:07 +0000

    Add legend setting function

--- R/legend_grob.R
+++ R/legend_grob.R
@@ -3,7 +3,9 @@
 #' This function used to create legends for the forest plot.
 #'
 #' @param name Character string, Legend name.
+#' @param label legend labels (expressions).
 #' @param color Colors for the group
+#' @param pch Legend symbol.
 #' @param position Position of the legend, \code{"right"}, \code{"top"},
 #' \code{"bottom"}.
 #' @param hgap Horizontal gap between the legend entries,
@@ -15,14 +17,15 @@
 #' @param ... Other parameters, not used currently.
 #'
 #' @return A frame grob
-#' @export
 #'
 #'
 legend_grob <- function(name = "",
+                        label,
                         color,
                         position = c("right", "top", "bottom"),
                         hgap = unit(0.1, "lines"), #horizontal gap
                         vgap = unit(0.5, "lines"), #vertical gap
+                        pch = 15,
                         fontsize = 12,
                         fontfamily = "",
                         ...
@@ -40,9 +43,6 @@ legend_grob <- function(name = "",
                                    fontface = 'bold',
                                    fill = 'black'))
 
-  # Legend
-  leg_labs <- if(is.null(names(color))) rep("", length(color)) else names(color)
-
   if(position %in% c("top", "bottom")){
     by_row <- FALSE
     ncol <- length(color)
@@ -52,7 +52,7 @@ legend_grob <- function(name = "",
     ncol <- 1
   }
 
-  leg_grob <- legendGrob(leg_labs, pch=15, ncol = ncol,
+  leg_grob <- legendGrob(label, pch = pch, ncol = ncol,
                          do.lines = TRUE, byrow = by_row,
                          hgap = hgap, vgap = vgap,
                          gp = gpar(col = color,
@@ -76,4 +76,60 @@ legend_grob <- function(name = "",
              grob = leg_grob,
              side = "bottom")
   }
+}
+
+
+
+#' CI settings for group
+#'
+#' This is to set the point group name, symbol and color of the CI.
+#'
+#' @param name Title of the legend.
+#' @param position Position of the legend, \code{"right"}, \code{"top"},
+#' \code{"bottom"}.
+#' @param value Legend labels (expressions)
+#' @param pch Shape of the point symbol, see \code{\link[grid]{pointsGrob}}.
+#' @param color Colors for the group/confidence interval.
+#'
+#' @export
+#'
+set_legend <- function(name = "", position = NULL, value = NULL, pch = 15, color){
+
+  max_len <- list(value, pch, color)
+  max_len <- max(vapply(max_len, length, FUN.VALUE = 1L), na.rm = TRUE)
+
+  if(is.null(value))
+    value <- ""
+
+  value <- rep_len(value, max_len)
+  pch <- rep_len(pch, max_len)
+  color <- rep_len(color, max_len)
+
+  if(is.null(position))
+    position <- "right"
+
+  position <- match.arg(position, c("right", "top", "bottom"))
+
+  out <- list(name = name,
+              position = position,
+              label = value,
+              pch = pch,
+              color = color)
+
+  class(out) <- union("ciset", out)
+
+  return(out)
+}
+
+# Generate default grouping
+.gen_legend <- function(group_number){
+
+  col_set <- c("#e41a1c","#377eb8","#4daf4a","#984ea3","#ff7f00",
+               "#ffff33","#a65628","#f781bf","#999999")
+
+  list(name = "Group",
+       position = "right",
+       value = paste("Group", 1:group_number),
+       pch = 0:(group_number - 1),
+       color = col_set[1:group_number])
 }
