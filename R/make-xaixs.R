3d06989c06b6dababc5cfbfb0237b100b5f70028
Alimu Dayimu <alimdayim@hotmail.com>
2022-02-20 18:47:51 +0000

    Set ticks and xlim different for each CI column

--- R/make-xaixs.R
+++ R/make-xaixs.R
@@ -1,11 +1,26 @@
-# Make xaxis
-make_xaxis <- function(at = NULL, label_at = NULL, labels = NULL, gp = gpar(), xlim){
+#' Create xaxis
+#'
+#' This function used to xais for the forest plot.
+#'
+#' @param at Numerical vector, create ticks at given values.
+#' @param xlab X-axis label.
+#' @param is_exp If values is exponential.
+#' @param gp Graphical parameters for arrow.
+#' @param xlim Limits for the x axis as a vector length 2, i.e. c(low, high)
+#'
+#' @return A grob
+#'
+#' @keywords internal
+make_xaxis <- function(at, xlab = NULL, is_exp = FALSE, gp = gpar(), xlim){
 
-  if(is.null(at))
-    at <- pretty(xlim)
+  if(is_exp){
+    label_at <- log(round(exp(at), 1))
+    labels <- as.character(round(exp(at), 1))
+  }else {
+    label_at <- round(at, 1)
+    labels <- as.character(round(at, 1))
+  }
 
-  if(is.null(labels))
-    labels <- as.character(at)
 
   maj <- linesGrob(x = unit(c(min(xlim), max(xlim)), "native"),
                    y = unit(c(0.99, 0.99), "npc"),
@@ -19,12 +34,30 @@ make_xaxis <- function(at = NULL, label_at = NULL, labels = NULL, gp = gpar(), x
                        gp = gp,
                        name = "tick")
 
-  lab <- textGrob(labels, x = unit(label_at, "native"), y = maj_cord$yb - unit(1, "lines"),
+  lab <- textGrob(labels,
+                  x = unit(label_at, "native"),
+                  y = maj_cord$yb - unit(1, "lines"),
                   gp = gp,
-                  just="centre", rot=0, check.overlap=TRUE,
+                  check.overlap=TRUE,
                   name = "label")
 
-  grobTree(gList(maj, tick, lab),
-           vp = viewport(xscale = xlim),
-           name = "xaxis")
+  if(!is.null(xlab)){
+    xlab_gb <- textGrob(xlab,
+                        x = unit(ifelse(is_exp, 1, 0), "native"),
+                        y = maj_cord$yb - unit(2, "lines"),
+                        gp = gp,
+                        check.overlap=TRUE,
+                        name = "xlab")
+
+    grobTree(gList(maj, tick, lab, xlab_gb),
+             vp = viewport(xscale = xlim),
+             name = "xaxis")
+
+  }else{
+    grobTree(gList(maj, tick, lab),
+             vp = viewport(xscale = xlim),
+             name = "xaxis")
+  }
+
+
 }
--- R/utils.R
+++ R/utils.R
@@ -10,12 +10,12 @@
 #'
 #' @return A list.
 #'
-#' @export
+#' @keywords deprecated
 #'
 set_xaxis <- function(break_at, label_at = NULL, label_value = NULL){
 
-  if(!is.numeric(break_at))
-    stop("break_at must be a numeric vector.")
+  if(!is.numeric(break_at) || (!missing(label_at) && !is.numeric(label_at)))
+    stop("break_at and label_at must be a numeric vector.")
 
   if(is.null(label_at))
     label_at <- break_at
@@ -39,6 +39,122 @@ set_xaxis <- function(break_at, label_at = NULL, label_value = NULL){
   class(r) <- union("xaxis", r)
 
   return(r)
+
+}
+
+
+#' Set x-axis ticks
+#'
+#' Create ticks points.
+#'
+#' @param at Numerical vector, create ticks at given values.
+#' @param is_exp If values is exponential.
+#' @inheritParams forest
+#'
+#' @return A vector
+#'
+#' @keywords internal
+
+make_ticks <- function(at = NULL,
+                       xlim,
+                       refline = ifelse(is_exp, 1, 0),
+                       is_exp = FALSE){
+
+  if(is.null(at)){
+
+    if(!is_exp){
+      ticks_at <- pretty(xlim)
+    }else {
+      pt_cut <- pretty(range(c(xlim, refline)))
+      pt_cut <- round(exp(pt_cut), 1) # Keep 1 digits
+      ticks_at <- log(unique(pt_cut)) # avoid any duplicate
+      # Limit values inside xlim
+      ticks_at <- ticks_at[exp(ticks_at) <= max(exp(xlim))]
+    }
+
+  }else {
+    if(is_exp)
+      ticks_at <- log(at)
+    else
+      ticks_at <- at
+  }
+
+  ticks_at <- ticks_at[is.finite(ticks_at)]
+  ticks_at[ticks_at >= min(xlim) & ticks_at <= max(xlim)]
+
+}
+
+
+#' Create xlim
+#'
+#' Create xlim based on value ranges.
+#'
+#' @param gp_num Group number
+#' @param is_exp If values is exponential.
+#' @inheritParams forest
+#'
+#' @return A list
+#'
+#' @keywords internal
+#'
+make_xlim <- function(xlim = NULL,
+                      lower,
+                      upper,
+                      gp_num = 1,
+                      ref_line = ifelse(is_exp, 1, 0),
+                      ticks_at = NULL,
+                      is_exp = FALSE){
+
+  if(gp_num == 1 || !is.null(xlim)){
+
+    # Use range if missing
+    if(is.null(xlim)){
+      xlim <- range(c(unlist(lower),
+                      ifelse(is_exp, exp(ref_line), ref_line),
+                      unlist(upper)),
+                    na.rm = TRUE)
+    }
+      
+    if(is_exp){
+      if(min(xlim) == 0)
+        xlim[which.min(xlim)] <- min(c(unlist(lower),
+                                       exp(ref_line),
+                                       ticks_at),
+                                     na.rm = TRUE)
+      xlim <- log(xlim)
+
+    }
+
+    xlim <- list(xlim)
+
+  }else{
+
+    gp_list <- rep_len(1:gp_num, length(lower))
+
+    xlim <- lapply(1:gp_num, function(x){
+      sel_num <- gp_list == x
+
+      xlim <- range(c(unlist(lower[sel_num]),
+                      ifelse(is_exp, exp(ref_line), ref_line),
+                      unlist(upper[sel_num])),
+                    na.rm = TRUE)
+
+      if(is_exp){
+        if(min(xlim) == 0)
+          xlim[which.min(xlim)] <- min(c(unlist(lower[sel_num]),
+                                         exp(ref_line),
+                                         ticks_at),
+                                       na.rm = TRUE)
+        xlim <- log(xlim)
+
+      }
+      return(xlim)
+
+    })
+
+  }
+
+  return(xlim)
 
 }
 
