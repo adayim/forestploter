6da84ef67e5d82d879888dbf764383820f9bb9a0
Alimu Dayimu <alimdayim@hotmail.com>
2023-09-23 18:37:55 +0100

    FIX CRAN notice

--- R/forestploter.R
+++ R/forestploter.R
@@ -8,6 +8,6 @@
 #' @seealso \code{\link{grid}},\code{\link{gridExtra}}
 #' @importFrom gtable gtable_add_grob gtable_add_rows gtable_add_padding gtable_add_cols
 #' @importFrom gridExtra tableGrob ttheme_minimal
-#' 
-function()
-  NULL
+#' @name forestploter-package
+#' @docType package
+"_PACKAGE"
